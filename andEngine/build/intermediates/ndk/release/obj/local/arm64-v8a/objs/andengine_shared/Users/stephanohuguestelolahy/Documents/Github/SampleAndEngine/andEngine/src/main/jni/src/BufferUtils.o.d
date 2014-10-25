da9655bc0a6e9319e15e6b2472b4f02ffc20f55c
Stéphano Telolahy <stephano.telolahy@gmail.com>
2014-10-25 09:11:20 +0100

    initial commit

---
+++ andEngine/build/intermediates/ndk/release/obj/local/arm64-v8a/objs/andengine_shared/Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngine/src/main/jni/src/BufferUtils.o.d
@@ -0,0 +1,86 @@
+/Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngine/build/intermediates/ndk/release/obj/local/arm64-v8a/objs/andengine_shared//Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngine/src/main/jni/src/BufferUtils.o: \
+ /Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngine/src/main/jni/src/BufferUtils.cpp \
+ /Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngine/src/main/jni/src/BufferUtils.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/jni.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/android/api-level.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/stdio.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/sys/types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/stdint.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/machine/wchar_limits.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/linux/types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/asm/types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/asm-generic/types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/asm-generic/int-ll64.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/asm/bitsperlong.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/asm-generic/bitsperlong.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/linux/posix_types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/linux/stddef.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/linux/compiler.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/asm/posix_types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/asm-generic/posix_types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/sys/sysmacros.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/stdlib.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/string.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/malloc.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/xlocale.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/alloca.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/strings.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/memory.h
+
+/Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngine/src/main/jni/src/BufferUtils.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/jni.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/android/api-level.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/stdio.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/sys/types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/stdint.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/machine/wchar_limits.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/linux/types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/asm/types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/asm-generic/types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/asm-generic/int-ll64.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/asm/bitsperlong.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/asm-generic/bitsperlong.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/linux/posix_types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/linux/stddef.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/linux/compiler.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/asm/posix_types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/asm-generic/posix_types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/sys/sysmacros.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/stdlib.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/string.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/malloc.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/xlocale.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/alloca.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/strings.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-21/arch-arm64/usr/include/memory.h:
