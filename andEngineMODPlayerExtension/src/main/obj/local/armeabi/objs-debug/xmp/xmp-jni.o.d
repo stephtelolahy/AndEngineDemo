e959a81d1e5d6e2a3347dead0f457874a8183045
Stéphano Telolahy <stephano.telolahy@gmail.com>
2014-10-28 13:38:38 +0000

    integrate extensions

---
+++ andEngineMODPlayerExtension/src/main/obj/local/armeabi/objs-debug/xmp/xmp-jni.o.d
@@ -0,0 +1,134 @@
+/Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngineMODPlayerExtension/src/main/obj/local/armeabi/objs-debug/xmp/xmp-jni.o: \
+ /Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngineMODPlayerExtension/src/main/jni/xmp-jni.c \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/strings.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/memory.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/jni.h \
+ /Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngineMODPlayerExtension/src/main/jni/include/xmp.h \
+ /Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngineMODPlayerExtension/src/main/jni/include/common.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/signal.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/limits.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/ucontext.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/user.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h \
+ /Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngineMODPlayerExtension/src/main/jni/include/xmp.h \
+ /Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngineMODPlayerExtension/src/main/jni/include/xxm.h \
+ /Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngineMODPlayerExtension/src/main/jni/include/list.h
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/string.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngineMODPlayerExtension/src/main/jni/include/xmp.h:
+
+/Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngineMODPlayerExtension/src/main/jni/include/common.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/ucontext.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/user.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h:
+
+/Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngineMODPlayerExtension/src/main/jni/include/xmp.h:
+
+/Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngineMODPlayerExtension/src/main/jni/include/xxm.h:
+
+/Users/stephanohuguestelolahy/Documents/Github/SampleAndEngine/andEngineMODPlayerExtension/src/main/jni/include/list.h:
